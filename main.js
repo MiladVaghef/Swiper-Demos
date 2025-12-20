38c194b9ee2924e217d31dec1ed3ab9e1941e15a
Milad Vaghef <143057362+MiladVaghef@users.noreply.github.com>
2025-12-20 06:03:32 +0330

    Feat: Add slider to the main root container
    Added a slider for the main container that's going to hold the whole project.
    
    It is a pagination with progress slider.

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 
+addEventListener("DOMContentLoaded", mainSlider);
+
+function mainSlider() {
+  const swiper = new Swiper("#main-slider", {
+    initialSlide: 1, // ✅ start from slide 1
+
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
